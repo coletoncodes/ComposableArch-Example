c76f4324b58c26542092940a9d90d0fc71f230b4
Coleton Gorecke <coletoncodes@gmail.com>
2024-03-30 12:12:57 -0500

    Initial Commit

---
+++ ComposableArch-Example/ComposableArch_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableArch_ExampleApp.swift
+//  ComposableArch-Example
+//
+//  Created by Coleton Gorecke on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ComposableArch_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
