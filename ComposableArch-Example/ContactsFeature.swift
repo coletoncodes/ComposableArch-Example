b672ab7d8e868ef8b8d5088f145b79bf09122b42
Coleton Gorecke <coletoncodes@gmail.com>
2024-03-30 22:39:54 -0500

    Added ContactsFeature

---
+++ ComposableArch-Example/ContactsFeature.swift
@@ -0,0 +1,76 @@
+//
+//  ContactsFeature.swift
+//  ComposableArch-Example
+//
+//  Created by Coleton Gorecke on 3/30/24.
+//
+
+import ComposableArchitecture
+
+struct Contact: Equatable, Identifiable {
+    let id: UUID
+    var name: String
+}
+
+@Reducer
+struct ContactsFeature {
+    @ObservableState
+    struct State: Equatable {
+        var contacts: IdentifiedArrayOf<Contact> = []
+    }
+    
+    enum Action {
+        case addButtonTapped
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .addButtonTapped:
+                return .none
+            }
+        }
+    }
+}
+
+import SwiftUI
+
+struct ContactsView: View {
+    let store: StoreOf<ContactsFeature>
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(store.contacts) {
+                    Text($0.name)
+                }
+            }
+            .navigationTitle("Contacts")
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        store.send(.addButtonTapped)
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContactsView(
+        store: Store(
+            initialState: ContactsFeature.State(
+                contacts: [
+                    Contact(id: UUID(), name: "Blob"),
+                    Contact(id: UUID(), name: "Blob Jr"),
+                    Contact(id: UUID(), name: "Blob Sr"),
+                ]
+            )
+        ) {
+            ContactsFeature()
+        }
+    )
+}
