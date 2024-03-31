aeb8240a70f98b0f95e8603ec3014b1fc2f5ae3a
Coleton Gorecke <coletoncodes@gmail.com>
2024-03-30 19:57:30 -0500

    Added CounterFeature

--- ComposableArch-Example/ComposableArch_ExampleApp.swift
+++ ComposableArch-Example/ComposableArch_ExampleApp.swift
@@ -5,13 +5,19 @@
 //  Created by Coleton Gorecke on 3/30/24.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct ComposableArch_ExampleApp: App {
+    static let counterViewStore = Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+            ._printChanges()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CounterView(store: ComposableArch_ExampleApp.counterViewStore)
         }
     }
 }
